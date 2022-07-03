056cc9f2b23f55ea4a1d372bc8372c61f5166345
kururu-abdo <wdalhajana50@gmail.com>
2022-07-04 00:46:41 +0200

    last

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 ThemeData dark = ThemeData(
   fontFamily: 'Cairo',
-  //primaryColor: Color(0xFFf1ed70),
-  primaryColor: Color(0xFFf1f352),
+  primaryColor: Color(0xFFe1eb99),
+ cardColor:Colors.black,
   brightness: Brightness.dark,
   highlightColor: Color(0xFF252525),
   hintColor: Color(0xFFc7c7c7),
+    colorScheme: ColorScheme.dark(primary: Color(0xFFe1eb99), secondary: Color(0xFFe1eb99)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(primary: Color(0xFFe1eb99))),
   pageTransitionsTheme: PageTransitionsTheme(builders: {
     TargetPlatform.android: ZoomPageTransitionsBuilder(),
     TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 ThemeData light = ThemeData(
   fontFamily: 'Cairo',
-  //  primaryColor: Color(0xFFf1ed70),
-primaryColor:Color(0xFFa3a25f),
+    primaryColor: Color(0xFFa3a25f),
+
+  cardColor:Colors.white,
   brightness: Brightness.light,
   highlightColor: Colors.white,
   hintColor: Color(0xFF9E9E9E),
+  colorScheme: ColorScheme.light(
+    onPrimary: Colors.black,
+    primary: Color(0xFFa3a25f), secondary: Color(0xFFa3a25f)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(primary: Color(0xFFa3a25f))),
   pageTransitionsTheme: PageTransitionsTheme(builders: {
     TargetPlatform.android: ZoomPageTransitionsBuilder(),
     TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
